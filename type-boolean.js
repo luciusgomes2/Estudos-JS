12fa1412bf0306477cebe319e50e692283c8a99c
lucius_gomes2 <lucius_gomes2@outlook.com>
2023-03-21 21:10:41 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,8 @@
+const num1 = 1;
+const num2 = 3;
+
+console.log(num1 === num2);
+
+const texto1 = "Lúcius";
+const texto2 = "Carol";
+console.log(texto1===texto2);
---
+++ type-string.js
@@ -0,0 +1,10 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input);
+
+const inputToLowerCase = input.toLocaleLowerCase();
+
+console.log(cidade === inputToLowerCase);
+
+console.log(inputToLowerCase.length);
